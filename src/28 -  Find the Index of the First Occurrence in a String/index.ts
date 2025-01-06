2100d23f330c96e4c34c68d45915add1fd6d0c26
Erik Mota Galindo <kenshimota@gmail.com>
2025-01-06 06:27:07 -0300

    the last changed

---
+++ src/28 -  Find the Index of the First Occurrence in a String/index.ts
@@ -0,0 +1,25 @@
+function strStr(haystack: string, needle: string): number {
+    let counted = 0;
+
+    for(let i = 0; i <= haystack.length; i++){
+        if(counted === needle.length) {
+            return i - counted;
+        }
+        
+        const c = haystack[i];
+
+        if(c === needle[counted]){
+            counted += 1;
+            continue;
+        }
+
+        i = i - counted ;
+        counted = 0;
+    }
+
+
+    return -1;
+};
+
+
+export default strStr;
